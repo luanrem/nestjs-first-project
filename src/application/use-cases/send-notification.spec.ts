ea65fc39bea619529258edec983d6b7e9063d45e
luanrem <luanrem@gmail.com>
2023-02-13 19:55:16 -0300

    feat: Create repository layer

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,18 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+    console.log(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     sendNotification.execute({
       content: 'This is a notification',
@@ -10,6 +20,6 @@ describe('Send notification', () => {
       recipientId: 'example-recipient-id',
     });
 
-    expect(sendNotification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
